a36fdfd5ad6a4a7528e4b153565e7c7d1c0d5423
Rafael Santos <rafaelnsantos1@gmail.com>
2017-11-15 18:58:17 -0200

    add import plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,12 @@ module.exports = {
     node: true
   },
   // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
-  extends: 'standard',
+  extends: [
+    'standard'
+  ],
+  plugins: [
+    'import'
+  ],
   // add your custom rules here
   'rules': {
     // allow paren-less arrow functions
@@ -12,6 +17,16 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // 'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2
+  },
+  'settings': {
+    'import/resolver': {
+      'babel-plugin-root-import': {}
+    }
   }
 }
