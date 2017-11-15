0b393d2ce9356e71ff465a4466e9c7c541469502
Rafael Santos <rafaelnsantos1@gmail.com>
2017-11-15 01:40:16 -0200

    added graphql-s2s

--- schema.js
+++ schema.js
@@ -1,11 +1,13 @@
 // https://hackernoon.com/graphql-schemas-easier-better-structure-31677a640d18
 import { makeExecutableSchema } from 'graphql-tools'
+import { graphqls2s } from 'graphql-s2s'
 
 import { glue } from 'schemaglue'
 
+const { transpileSchema } = graphqls2s
 const { schema, resolver } = glue('src/graphql')
 
 export default makeExecutableSchema({
-  typeDefs: schema,
+  typeDefs: [transpileSchema(schema)],
   resolvers: resolver
 })
