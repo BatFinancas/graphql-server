b23dbe28422469fbef9d6b17d9e462fd8c64153c
Rafael Santos <rafaelnsantos1@gmail.com>
2017-11-16 04:32:44 -0200

    remove line

--- schema.js
+++ schema.js
@@ -1,7 +1,6 @@
 // https://hackernoon.com/graphql-schemas-easier-better-structure-31677a640d18
 import { makeExecutableSchema } from 'graphql-tools'
 import { graphqls2s } from 'graphql-s2s'
-
 import { glue } from 'schemaglue'
 
 const { transpileSchema } = graphqls2s
